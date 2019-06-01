c378523fcbb1813bced051888122be0911206aee
Kevin Boyle <papershack@gmail.com>
2019-05-31 17:57:11 -0700

    create state and pass as props for manipulating Counter component

--- src/App.js
+++ src/App.js
@@ -18,26 +18,51 @@ const secondTooltip = (
   </Tooltip>
 )
 
-function App() {
-  return (
-    <main className='App'>
-      <Messages name="Messages" unread={0}/>
-      <Messages name="Notifications" unread={10}/>
-      <Split className='left' flexBasis='2'>
-        This is the content for the left Split. Lorem {firstTooltip} dolor sit amet consectetur, adipisicing elit. Incidunt ex velit suscipit facere officia?<br />
-        {/* make another tooltip directly inside the App */}
-        <Tooltip message='one more tooltip message'>
-          Necessitatibus?
-        </Tooltip>
-      </Split>
-      <Split className='right'>
-        This is the content for the right Split. Inventore aliquid cupiditate suscipit repellat. Quaerat quis {secondTooltip} quam fuga. Aliquid quo possimus id soluta aspernatur.
-      </Split>
-      <TheDate />
-      <Counter />
-    </main>
+class App extends React.Component {
+  state = {
+    stepInput: 1,
+    step: 1
+  }
 
-  )
+  handleInput = e => {
+    this.setState({
+      stepInput: e.target.value
+    })
+  }
+
+  handleStepSubmission(){
+    this.setState({
+      step: Number(this.state.stepInput)     
+    })
+    this.setState({
+      stepInput: ''
+    })
+    console.log(this.state.step)
+  }
+  render(){
+    return (
+        <main className='App'>
+          <Messages name="Messages" unread={0}/>
+          <Messages name="Notifications" unread={10}/>
+          <Split className='left' flexBasis='2'>
+            This is the content for the left Split. Lorem {firstTooltip} dolor sit amet consectetur, adipisicing elit. Incidunt ex velit suscipit facere officia?<br />
+            {/* make another tooltip directly inside the App */}
+            <Tooltip message='one more tooltip message'>
+              Necessitatibus?
+            </Tooltip>
+          </Split>
+          <Split className='right'>
+            This is the content for the right Split. Inventore aliquid cupiditate suscipit repellat. Quaerat quis {secondTooltip} quam fuga. Aliquid quo possimus id soluta aspernatur.
+          </Split>
+          <TheDate />
+          <div className="step-container">
+            <input type="number" onChange={(e) => this.handleInput(e)} value={this.state.stepInput}/>
+            <button onClick={() => this.handleStepSubmission()}>Submit</button>
+            <Counter step={this.state.step} />
+          </div>
+        </main>
+      )
+    }
 }
 
 export default App
--- src/state/Counter.js
+++ src/state/Counter.js
@@ -1,26 +1,50 @@
-import React from 'react'
+import React from 'react';
+import PropTypes from 'prop-types'
 
 
 class Counter extends React.Component {
-    state = {
-        count: 0
+    constructor(props){
+        console.log(props)
+        super(props)
+        this.state = {
+            count: 0
+        }
     }
 
-    handleButtonClick= () => {
-        const newCount = ++this.state.count
+    handleIncrementButtonClick= () => {
+        console.log('props: ', this.props)
+        console.log('state: ', this.state)
+        const newCount = this.state.count + this.props.step;
         this.setState({
             count: newCount
         })
     }
 
+    handleDecrementButtonClick = () => {
+        const newCount = this.state.count - this.props.step;
+        this.setState({
+            count: newCount
+        })
+    }
+
+
     render(){
         return (
             <div>
                 <p>Current Count: {this.state.count}</p>
-                <button onClick={() => this.handleButtonClick()}>Add One</button>
+                <button onClick={() => this.handleIncrementButtonClick()}>Add {this.props.step}</button>
+                <button onClick={() => this.handleDecrementButtonClick()}>Take {this.props.step} Away</button>
             </div>
         );
     }
+
+    static defaultProps = {
+        step: 1
+    }
+}
+
+Counter.propTypes = {
+    step: PropTypes.number
 }
 
 export default Counter;
